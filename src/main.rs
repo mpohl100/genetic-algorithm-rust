62a98c91d1aeaab630956d2c9905c8de16964fa4
Michael Pohl <mpohl100@googlemail.com>
2023-12-07 01:51:10 +0000

    added vscode config. implemented draw_circle and adjusted test

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,14 @@ mod math2d;
 
 use crate::canvas::Canvas;
 use crate::evol::ordinary_evol::EvolutionOptions;
+use crate::math2d::circle::Circle;
+use crate::math2d::point::Point;
 
 fn main() {
     println!("Hello, world!");
     let options = EvolutionOptions::new();
-    let mut canvas = Canvas::new(10, 10);
+    let mut canvas = Canvas::new(40, 40);
+    canvas.draw_circle(Circle::new(Point::new(20.0, 20.0), 10.0));
+    let canvas_pixels = canvas.get_pixels();
+    print!("{}", canvas_pixels);
 }
